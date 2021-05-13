88e3542f3634b438710d5a79af55a8a33709f050
XuGuohui <xghui999@126.com>
2021-05-13 17:47:38 +0800

    minor

--- include/gsm0710muxer/muxer_impl.h
+++ include/gsm0710muxer/muxer_impl.h
@@ -410,11 +410,13 @@ inline int Muxer<StreamT, MutexT>::resumeChannel(uint8_t channel) {
 
     GSM0710_LOG_DEBUG(INFO, "Resuming channel %d", channel);
 
-    std::lock_guard<MutexT> lk(mutex_);
+    {
+        std::lock_guard<MutexT> lk(mutex_);
 
-    const auto newState = (c->localModemState | ((proto::RTR | proto::RTC))) & ~(proto::FC);
-    c->localModemState = newState;
-    c->update = true;
+        const auto newState = (c->localModemState | ((proto::RTR | proto::RTC))) & ~(proto::FC);
+        c->localModemState = newState;
+        c->update = true;
+    }
 
     xEventGroupSetBits(events_, EVENT_WAKEUP);
 
